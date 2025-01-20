b3941646ee90b8c69c0e03d069ad2beec84ea843
stephipie <stephi.pietschmann@gmail.com>
2025-01-20 14:58:39 +0100

    add feature branch to implement data persistence with sqlite3

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const database = require("better-sqlite3");
 const app = express();
 const port = process.env.NOTES_API_PORT || 8080;
 
@@ -21,62 +22,122 @@ let notes = [
   },
 ];
 
-app.listen(port, () => {
-  console.log(`server running on http://localhost:${port}`);
-});
+// connect to the database
+const db = new database("notes.db", { verbose: console.log });
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
-});
+// create a table if not exist
+db.exec(`
+  CREATE TABLE IF NOT EXISTS notes (
+  id INTEGER PRIMERY KEY AUTOINCREMENT, 
+  note TEXT,
+  author TEXT,
+  date TEXT)
+  `);
 
-app.get("/notes", (request, response) => {
-  response.json(notes);
-});
-
-app.get("/notes/:id", (request, response) => {
-  const id = parseInt(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+  
+  app.get("/", (request, response) => {
+    response.send("Hello World");
+  });
+  
+  // get all notes
+  app.get("/notes", (request, response) => {
+    // SELECT * FROM todos;
+    const rows = db.prepare("SELECT * FROM todos").all();
+    response.json(rows);
+  });
+  
+  // get single todo by id
+  app.get("/notes/:id", (request, response) => {
+    // SELECT * FROM notes WHERE id = ?;
+    const id = parseInt(request.params.id);
+    const note = db.prepare("SELECT * FROM notes WHERE id = ?").get(id);
+    // const note = notes.find((note) => note.id === id);
+    // if (note) {
+    //   response.json(note);
+    // } else {
+    //   response.status(404).json({ message: `Note with id ${id} not found` });
+    // }
+    // Early return if note is not found
+    if (!note) {
+      return response.status(404).json({ message: "Note not found"});
+    }
     response.json(note);
-  } else {
-    response.status(404).json({ message: `Note with id ${id} not found` });
-  }
-});
+  });
+  
+  // Create a new note
+  app.post("/notes", (request, response) => {
+    // INSERT INTO notes (note, author, date) VALUES (?, ?, ?);
+    const { note, author, date } = request.body;
+    const insert = db.prepare("INSERT INTO notes (note, author, date) VALUES (?, ?, ?)");
+    insert.run(text, text, text);
 
-app.post("/notes", (request, response) => {
-  const lastId = notes.length > 0 ? notes[notes.length - 1].id : 0;
-  const newNote = {
-    id: lastId + 1,
-    note: request.body.note,
-    author: request.body.author,
-    date: new Date(),
-  };
-  notes.push(newNote);
-  response.json(notes);
-});
+    const newNote = db
+    .prepare("SELECT * FROM todos ORDER BY id DESC LIMIT 1")
+    .get();
+    response.json(newNote);
+    // const lastId = notes.length > 0 ? notes[notes.length - 1].id : 0;
+    // const newNote = {
+    //   id: lastId + 1,
+    //   note: request.body.note,
+    //   author: request.body.author,
+    //   date: new Date(),
+    // };
+    // notes.push(newNote);
+    // response.json(notes);
+  });
+  
+  // update a note
+  app.put("/notes/:id", (request, response) => {
+    // UPDATE notes SET note = ?, author = ?, date = ? WHERE id = ?;
+    const id = parseInt(request.params.id);
+    const { text, text, text } = request.body;
+    
+    const exist = db.prepare("SELECT * FROM notes WHERE id = ?").get(id);
+    if (!exist) {
+      return response.status(404).json({ message: "Note not found"});
+    }
 
-app.put("/notes/:id", (request, response) => {
-  const id = parseInt(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    note.note = request.body.note;
-    note.author = request.body.author;
-    note.date = request.body.date;
-    response.json(notes);
-  } else {
-    response.status(404).json({ message: `Note with id ${id} not found` });
-  }
-});
+    db.prepare("UPDATE notes SET note = ?, author = ?, date = ? WHERE id = ?").run(
+      note,
+      author,
+      date,
+      id
+    );
 
-app.delete("/notes/:id", (request, response) => {
-  const id = parseInt(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  // notes.forEach((note) => {
-  //     let newId = parseInt(note.id);
-  //     if (id < newId) {
-  //         newId = newId - 1;
-  //         note.id = newId;
-  //     };
-  // });
-  response.json(notes);
-});
+    const note = db.prepare("SELECT * FROM notes WHERE id = ?").get(id);
+    response.json(note);
+    // const note = notes.find((note) => note.id === id);
+    // if (note) {
+    //   note.note = request.body.note;
+    //   note.author = request.body.author;
+    //   note.date = request.body.date;
+    //   response.json(notes);
+    // } else {
+    //   response.status(404).json({ message: `Note with id ${id} not found` });
+    // }
+  });
+  
+  // Delete a note
+  app.delete("/notes/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    // DELETE FROM notes WHERE id = ?;
+    const exist = db.prepare("SELECT * FROM notes WHERE id = ?").get(id);
+    if (!exist) {
+      return response.status(404).json({ message: "Note not found"});
+    }
+    const deleteNote = db.prepare("DELETE FROM notes WHERE id = ?").run(id);
+    response.json({ message: "Note is deleted" });
+    // notes = notes.filter((note) => note.id !== id);
+    // notes.forEach((note) => {
+      //     let newId = parseInt(note.id);
+      //     if (id < newId) {
+        //         newId = newId - 1;
+        //         note.id = newId;
+        //     };
+        // });
+        // response.json(notes);
+      });
+      
+      app.listen(port, () => {
+        console.log(`server running on http://localhost:${port}`);
+      });
